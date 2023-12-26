9e11b9ab04d9c561b306fabdee77797382f86e3f
Edwin Wong <wongedwin93@gmail.com>
2023-12-27 00:00:16 +0800

    convert queue to stack

--- Bye/src/bye/MySourProgram.java
+++ Bye/src/bye/MySourProgram.java
@@ -3,25 +3,21 @@ package bye;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class MySourProgram {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		Scanner in = new Scanner(new File("students.txt"));
+		LinkedList<String> names = new LinkedList<String>();
+		names.push("Caleb");
+		names.push("Sue");
+		names.push("Sally");
 		
-		List<String> students = new ArrayList<String>();
-		
-		while(in.hasNextLine()) {
-			students.add(in.nextLine());
-		}
-		
-		for(int i = 0; i < students.size(); i++) {
-			System.out.println("name: " + students.get(i));
-		}
-		
-		in.close();
+		System.out.println(names.remove());
+		System.out.println(names.remove());
+		System.out.println(names.remove());
 	}
 
 }
